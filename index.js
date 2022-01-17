d5eb16655525789fdf1b57f3bcc9dc158d8811bd
Rebekah Zhou <rebekahzhou@gmail.com>
2022-01-17 15:53:15 -0800

    commit tree 01b649cff0457f5c47d252cd42386147e82180a6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.getElementById("text")
+    element.textContent = "This is really cool!"
+    console.log('Hi')
+})
